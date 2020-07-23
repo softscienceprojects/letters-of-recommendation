ec500caf335f8993f81a53af944e103c42d26d25
erin nicole johnson <erin.nicole.johnson@gmail.com>
2020-07-23 22:20:05 +0100

    forward/back

--- app/static/js/post.js
+++ app/static/js/post.js
@@ -1,1 +1,5 @@
-console.log("I'm here!")
+images = document.querySelectorAll('.slideshow-image')
+captions = document.querySelectorAll('.slideshow-caption')
+
+images[0].style.display="inline-block"
+"inline-block"
